32e788872fb50d4f650ccdfa14da38d46d89c9f3
TUPAK SAUL CACHIMUEL COLTA <tscachimuelc@utn.edu.ec>
2025-12-02 12:45:40 -0500

    GestionarTorneosAPI

---
+++ GestionTorneosAPI/Program.cs
@@ -0,0 +1,41 @@
+
+namespace GestionTorneosAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<GestionTorneosAPIContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("ZoologicoAPIContext.postgresql") ?? throw new InvalidOperationException("Connection string 'GestionTorneosAPIContext' not found."))
+
+            );
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
